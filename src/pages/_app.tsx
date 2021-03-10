57c3373366370478c837a7128dcba823ecd5f798
Vinicius Coelho <vinicius_coelho@live.com>
2021-03-10 18:07:40 -0400

    refactor: remove theme provider from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/global';
-import theme from '../styles/themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
